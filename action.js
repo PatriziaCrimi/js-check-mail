7909bb1a780d613f5d05106329718ef6e0811190
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-14 15:48:19 +0200

    Completato algoritmo base con output in console

--- action.js
+++ action.js
@@ -1,7 +1,15 @@
-/* INSTRUCTIONS
-Creare un array con un insieme di indirizzi email.
-Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
-Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
-*/
+// Variables and constants initialization
+var email_list = ['patrizia@alice.it', 'moira@tiscali.it', 'giovanni@libero.it', 'cinzia@gmail.com', 'mel@email.com', 'laura@virgilio.it', 'luigi@gmail.com', 'juan@alice.it', 'valentina@gmail.com'];
+var user_email = prompt('Please enter your e-mail address.');
 
-// Variables and constants inizialization
+// ***************** Console input check *****************
+console.log('This is your array: ' + email_list);
+console.log('This array contains ' + email_list.length + ' elements.');
+console.log('The user e-mail is: ' + user_email);
+
+// ***************** Email check *****************
+for (var i = 0; i < email_list.length; i++) {
+  if (user_email === email_list[i]) {
+    console.log('Your e-mail ' + user_email + ' is already on our mailing list.');
+  }
+}
