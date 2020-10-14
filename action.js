852110518ad41a9966067264d55273022e62e2b5
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-14 14:25:21 +0200

    Impostati file

---
+++ action.js
@@ -0,0 +1,7 @@
+/* INSTRUCTIONS
+Creare un array con un insieme di indirizzi email.
+Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
+Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
+*/
+
+// Variables and constants inizialization
